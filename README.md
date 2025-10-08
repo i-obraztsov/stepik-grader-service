838fc9be14ad6ed2a2a63d0502e4371ca502343b
Ivan Obraztsov <o6raztsv@gmail.com>
2025-10-08 15:10:26 +0200

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# stepik-grader-service
+
+
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+// src/index.ts
+function greet(name: string): string {
+  return `Hello, ${name}!`;
+}
+
+const userName: string = "TypeScript User";
+console.log(greet(userName));
